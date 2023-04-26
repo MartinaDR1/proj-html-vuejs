feeade0dfa0f6732464814de14f76d74f2987a8f
MartinaDR1 <derosemartina933@gmail.com>
2023-04-26 16:09:32 +0200

    add appHeader

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,88 @@
+<script>
+import Jumbo from "../components/Jumbo.vue";
+export default {
+    name: 'AppHeader',
+    data() {
+        return{
+            itemNav: [
+                {
+                    title: "home",
+                    link: "#"
+                },
+                {
+                    title: "courses",
+                    link: "#"
+                },
+                {
+                    title: "instructors",
+                    link: "#"
+                },
+                {
+                    title: "events",
+                    link: "#"
+                },
+                {
+                    title: "pages",
+                    link: "#"
+                },
+                {
+                    title: "elements",
+                    link: "#"
+                }
+            ]
+        }
+    },
+    components:{
+        Jumbo
+    }
+}
+</script>
+
+<template>
+    <Jumbo/>
+    <nav class="container d-flex justify-content-around py-1">
+        <div class="logo py-2">
+            <img src="../assets/img/logo-light.png" alt="">
+        </div>
+
+        <div class="item d-flex py-3">
+            <ul v-for="item in itemNav">
+                <li><a href="" class="text-uppercase" >{{ item.title }}</a></li>
+            </ul>
+        </div>
+
+        <div class="icon py-3">
+            <i class="fa-solid fa-magnifying-glass"></i>
+            <i class="fa-solid fa-bag-shopping"></i>
+            <i class="fa-solid fa-bars"></i>
+        </div>
+    </nav>
+</template>
+
+
+<style lang="scss" scoped>
+@use '../assets/scss/variables'as *;
+nav{
+    position: absolute;
+    top: 1rem;
+    left: 7rem;
+    .logo img{
+        width: 150px;
+    }
+
+    ul{
+    list-style: none;
+
+        a{
+            text-decoration: none;
+            font-size: 12px;
+            color: $txt-dark_light
+        }
+    }   
+    .icon i{
+        padding-right: 1rem;
+        color: $txt-dark_light
+    }
+}
+
+</style>
---
+++ src/components/Jumbo.vue
@@ -0,0 +1,48 @@
+<script>
+    export default {
+        name: 'Jumbo'
+        
+    }
+</script>
+
+<template>
+<div class="jumbo">
+    <div class="sm_container text">
+        <h1 class="p-3 fw-bold text-light">Contemporary Ideas</h1>
+        <p class="px-5">Lorem Ipsn gravida nibh vel velit auctor aliquetnean sollicitudin, lorem quis bibendum auci elit consequat ipsutis sem nibh id elited odio sit amet nibcursus a sit amet mauris.</p>
+        
+        <button type="submit" class="text-uppercase text-light fw-semibold border-0 my-4">register now</button>
+    </div>
+</div>
+</template>
+
+
+<style lang="scss" scoped>
+@use '../assets/scss/variables'as *;
+
+    .sm_container{
+        margin: auto;
+        width: 70%;
+    }
+    .jumbo{
+        background-image: url(../assets/img/h5-slide-3-background.jpg);
+        background-position: bottom;
+        height: 600px;
+
+        .text{
+            padding-top: 10rem;
+            text-align: center;
+            
+            p{
+                color: $txt-dark_light
+            }
+
+            button{
+                background-color: $bg-blue;
+                padding: 0.75rem 2rem;
+                font-size: 12px;
+            }
+        }   
+    }
+
+</style>
